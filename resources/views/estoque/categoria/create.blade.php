06efd26bbb379e97e63ef00948399421cbf615f1
joao <joao.silva@forseti.com.br>
2018-12-18 16:47:31 -0200

    primeiro commit

---
+++ resources/views/estoque/categoria/create.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joaosilva
+ * Date: 18/12/18
+ * Time: 16:22
+ */
+
+@extends('layouts.admin);
+@section('conteudo');
+
+<h3>Create</h3>
+
+@stop
---
+++ resources/views/estoque/categoria/search.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joaosilva
+ * Date: 18/12/18
+ * Time: 16:45
+ */
