a122ce5d6b4e17b462d82af385bc112d561d6e0b
joao <joao.silva@forseti.com.br>
2018-12-18 16:58:49 -0200

    listando categorias

--- resources/views/estoque/categoria/create.blade.php
+++ resources/views/estoque/categoria/create.blade.php
@@ -1,13 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: joaosilva
- * Date: 18/12/18
- * Time: 16:22
- */
-
-@extends('layouts.admin);
-@section('conteudo');
+@extends('layouts.admin)
+@section('conteudo')
 
 <h3>Create</h3>
 
